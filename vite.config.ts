341af36a63a33335902df7fdbc0bea60cd5da593
EricG442 <ericg206@outlook.com>
2026-04-25 22:50:25 -0700

    updated gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mariners_2026_schedule/"
+  base: "/mariners-2026-schedule/"
 })
